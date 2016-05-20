562ca070da3a77ebacb06df32e72f87a27f8d786
Andreas Aarsland <andreas@agens.no>
2016-05-20 12:59:47 +0200

    Support carthage

---
+++ SaneNSErrorTests/SaneNSErrorTests.m
@@ -0,0 +1,39 @@
+//
+//  SaneNSErrorTests.m
+//  SaneNSErrorTests
+//
+//  Created by Andreas Aarsland on 20.05.2016.
+//  Copyright © 2016 Agens. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SaneNSErrorTests : XCTestCase
+
+@end
+
+@implementation SaneNSErrorTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
